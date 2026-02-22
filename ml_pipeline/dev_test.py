c5044ad8bb4be6353d429990d9f8c2984c8b0d40
dtamez <zematynnad@gmail.com>
2026-02-21 22:20:15 -0600

    BPM detection "works" to some extent but is too simplistic and so is the signal (just delta pulses). Need to improve both.

--- ml_pipeline/dev_test.py
+++ ml_pipeline/dev_test.py
@@ -1,21 +1,29 @@
 from synthetic_signals import pulse_train
-from features import python_rms
+from features import python_rms, detect_peaks
 from visualization import plot_signal_and_rms
 from rust_bridge import rms_batch
 
 bpm = 120
-signal = pulse_train(bpm=bpm, duration_sec=5)
+signal = pulse_train(bpm=bpm, duration_sec=10)
 
 frame_size = 1024
 hop_size = 512
 rms_envelope = rms_batch(signal, frame_size, hop_size)
 
-plot_signal_and_rms(
-    signal,
-    rms_envelope,
-    hop_size=hop_size,
-    title="Synthetic Beat Signal + RMS Envelope",
-)
+peaks = detect_peaks(rms_envelope)
+
+print(peaks[:10])
+print("Total peaks:", len(peaks))
+
+for i in peaks[:5]:
+    print(rms_envelope[i - 2 : i + 3])
+
+# plot_signal_and_rms(
+#     signal,
+#     rms_envelope,
+#     hop_size=hop_size,
+#     title="Synthetic Beat Signal + RMS Envelope",
+# )
 
 # energy = rms_batch(signal, frame_size, hop_size)
 #
--- ml_pipeline/features.py
+++ ml_pipeline/features.py
@@ -16,3 +16,16 @@ def python_rms(values):
 
 def rust_rms(values):
     return get_energy(values)
+
+
+def detect_peaks(rms_envelope):
+    # takes an array of values and outputs a subset of those values
+    # a value counts as a peak if it is greater than both
+    # the previous and the subsequnent sample
+    output = []
+    for idx, val in enumerate(rms_envelope):
+        if idx == 0 or idx == len(rms_envelope) - 1:
+            continue
+        if val > rms_envelope[idx - 1] and val > rms_envelope[idx + 1]:
+            output.append(idx)
+    return output
